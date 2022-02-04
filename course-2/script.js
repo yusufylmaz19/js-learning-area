e1fc3aca1dc206077e5081ddb8ad0938c79365da
yusufylmaz19 <yusufylmaz15@gmail.com>
2022-02-04 17:38:36 +0300

    completing bj game

--- course-2/script.js
+++ course-2/script.js
@@ -5,30 +5,43 @@ document.querySelector("#btn-2").addEventListener("click", saveCount);
 var count=0;
 var h2=document.getElementById('count-el');
 var div=document.getElementById('result-div');
-div.innerText='Previous entries: ';
+div.textContent='Previous entries: ';
 function incrementNumber(){
     count++;
-    h2.innerText=count;
+    h2.textContent=count;
 }
 
 function saveCount(){
   div.textContent+=  count + ' - ';
   count=0;
-  h2.innerText=count;
+  h2.textContent=count;
 } 
+
+
 
 //  challange 2
 
+
 var getRandomCard=()=> {
+  var rnd=Math.floor(Math.random()*13)+1;
+  if(rnd>10){
+    rnd=10;
+  }
+    else if(rnd===11 || rnd===1){
+    if(sum + rnd <=21){
+      rnd=11;
+     }else{
+       rnd=1;
  
-  return  Math.floor(Math.random()*11)+1;
+     }
+  }
+  return  rnd;
 }
 
 
-let firstCard=getRandomCard();
-let secondCard=getRandomCard();
-let cards=[firstCard,secondCard];
-let sum=firstCard+secondCard;
+
+let cards=[];
+let sum=0;
 let hasbBlackJacke=false;
 let isAlive=true;
 let message='';
@@ -42,13 +55,19 @@ document.querySelector('#btn-3').addEventListener('click',startGame);
 document.querySelector('#btn-4').addEventListener('click',newCard);
 
 function startGame(){
+  isAlive=true;
+  let firstCard=getRandomCard();
+  let secondCard=getRandomCard();
+  cards=[firstCard,secondCard];
+  sum=firstCard+secondCard;
   renderGame();
 }
 
 function renderGame(){
-    cardEl.textContent='Card:'+cards[0]+'-'+cards[1];   
+    cardEl.textContent='Card:';
+    showCards();
     sumEl.textContent= sum;
-       if(sum<20){
+       if(sum<=20){
           message='Do you want to new card?';
        }else if(sum===21){
           message="Whoo!! You've got a Blackjake!";
@@ -62,15 +81,17 @@ function renderGame(){
 
 
 function newCard(){
+  if(isAlive===true && hasbBlackJacke===false){
   let card=getRandomCard();
   sum +=card;
-  renderGame();
   cards.push(card);
-  showCards();
-
+  renderGame();
+}
 }
 
 function showCards (){
-  console.info(cards);
+   for(let i=0;i<cards.length; i++){
+     cardEl.textContent +=cards[i] +' ';
+   }
 }
 
